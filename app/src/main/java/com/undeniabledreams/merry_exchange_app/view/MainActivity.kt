2a06e8493681511236677896396258391c45eae8
Yakusugi <yosukeito1987@gmail.com>
2023-08-14 21:17:01 +0900

    commit august 14 2023 HomeFragment.kt ProfileFragment.kt ChatFragment.kt

--- app/src/main/java/com/undeniabledreams/merry_exchange_app/view/MainActivity.kt
+++ app/src/main/java/com/undeniabledreams/merry_exchange_app/view/MainActivity.kt
@@ -2,11 +2,42 @@ package com.undeniabledreams.merry_exchange_app.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.undeniabledreams.merry_exchange_app.R
+import com.undeniabledreams.merry_exchange_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+                R.id.chat -> replaceFragment(ChatFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
+
+
